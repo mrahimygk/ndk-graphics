8b0b6706907e0bbcd2994c9134d194ef303c8157
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-10 20:04:38 +0430

    providing interface to modify bitmaps by activity views

--- app/src/main/cpp/bitmap-sine.cpp
+++ app/src/main/cpp/bitmap-sine.cpp
@@ -3,12 +3,18 @@
 #include <android/bitmap.h>
 #include "bitmap-basic.cpp"
 
-void fill_bitmap(AndroidBitmapInfo *ptr, uint8_t *pixels) {
+void
+fill_bitmap(
+        AndroidBitmapInfo *ptr,
+        uint8_t *pixels,
+        jdouble height_position_ratio,
+        jdouble width_frequency
+) {
     float pi = 3.1415;
     for (float x = 0; x < pi; x += 0.01) {
         float y = sin(2 * pi * x);
-        float c = ptr->width / 6 * x;
-        float l = 80 * y + ptr->height / 3;
+        float c = ptr->width / width_frequency * x;
+        float l = 80 * y + ptr->height * height_position_ratio;
         set_pixel(ptr, pixels, c, l, 0xf5555555);
     }
 }
@@ -18,7 +24,7 @@ JNIEXPORT void JNICALL
 Java_ir_mrahimy_ndkgraphics_pages_SineActivity_00024BasicBitmapView_ndkRenderSine(
         JNIEnv *env,
         jobject thiz,
-        jobject bitmap) {
+        jobject bitmap, jdouble height_position_ratio, jdouble width_frequency) {
 
     AndroidBitmapInfo info = {0};
     uint8_t *pixelBuf;
@@ -27,7 +33,7 @@ Java_ir_mrahimy_ndkgraphics_pages_SineActivity_00024BasicBitmapView_ndkRenderSin
 
     AndroidBitmap_lockPixels(env, bitmap, (void **) &pixelBuf);
 
-    fill_bitmap(&info, pixelBuf);
+    fill_bitmap(&info, pixelBuf, height_position_ratio, width_frequency);
 
     AndroidBitmap_unlockPixels(env, bitmap);
 }
--- app/src/main/java/ir/mrahimy/ndkgraphics/pages/SineActivity.kt
+++ app/src/main/java/ir/mrahimy/ndkgraphics/pages/SineActivity.kt
@@ -1,11 +1,17 @@
 package ir.mrahimy.ndkgraphics.pages
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SineActivity : AppCompatActivity() {
@@ -16,6 +22,7 @@ class SineActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,7 +30,91 @@ class SineActivity : AppCompatActivity() {
         val displaySize = Point()
         display.getSize(displaySize)
 
-        setContentView(BasicBitmapView(this, displaySize.x, displaySize.y))
+        setContentView(
+            RelativeLayout(this).apply {
+                val bg = BasicBitmapView(this@SineActivity, displaySize.x, displaySize.y)
+                addView(bg)
+
+                val upButton = Button(this@SineActivity).apply {
+                    text = "\u2191"
+                    alpha = 0.75f
+                    setOnClickListener {
+                        bg.run {
+                            heightPositionRatio -= 0.01
+                            invalidate()
+                        }
+                    }
+                }
+
+                val downButton = Button(this@SineActivity).apply {
+                    text = "\u2193"
+                    alpha = 0.75f
+                    setOnClickListener {
+                        bg.run {
+                            heightPositionRatio += 0.01
+                            invalidate()
+                        }
+                    }
+                }
+
+                addView(downButton,
+                    RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE)
+                        addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+                    })
+
+                addView(upButton,
+                    RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE)
+                        addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
+                    })
+
+
+                val leftButton = Button(this@SineActivity).apply {
+                    text = "\u2190"
+                    alpha = 0.75f
+
+                    setOnClickListener {
+                        bg.run {
+                            widthFrequency += 0.1
+                            invalidate()
+                        }
+                    }
+
+                }
+
+                val rightButton = Button(this@SineActivity).apply {
+                    text = "\u2192"
+                    alpha = 0.75f
+                    setOnClickListener {
+                        bg.run {
+                            widthFrequency -= 0.1
+                            invalidate()
+                        }
+                    }
+                }
+
+                addView(rightButton,
+                    RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE)
+                        addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+                    })
+
+                addView(leftButton,
+                    RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE)
+                        addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
+                    })
+            }
+        )
     }
 
     class BasicBitmapView(
@@ -33,14 +124,22 @@ class SineActivity : AppCompatActivity() {
     ) : View(context) {
         private var mBitmap: Bitmap? = null
 
-        private external fun ndkRenderSine(bitmap: Bitmap?)
+        var heightPositionRatio: Double = 1.0 / 3.0
+        var widthFrequency: Double = 6.0
+
+        private external fun ndkRenderSine(
+            bitmap: Bitmap?,
+            heightPositionRatio: Double,
+            widthFrequency: Double
+        )
 
         init {
             mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
         }
 
         override fun onDraw(canvas: Canvas) {
-            ndkRenderSine(mBitmap)
+            mBitmap?.eraseColor(Color.BLACK)
+            ndkRenderSine(mBitmap, heightPositionRatio, widthFrequency)
             canvas.drawBitmap(mBitmap!!, 0f, 0f, null)
         }
     }
